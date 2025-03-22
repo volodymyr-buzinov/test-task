6db335b0d5b76f891567539c523b86f656781132
volodymyr <volodymyr-desktop@MacBook-Pro-Admin.local>
2025-03-22 18:56:57 +0200

    QUIZ | update posthog init options

--- frontend/app/_context/PosthogProvider.tsx
+++ frontend/app/_context/PosthogProvider.tsx
@@ -11,9 +11,6 @@ export function PHProvider({ children }: {children: ReactNode}) {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
       person_profiles: 'identified_only',
-      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
-      capture_pageleave: true,
-      persistence: 'memory',
     })
   }, [])
 
