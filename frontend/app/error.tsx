87df33822d1a3824711e30db86f6a1d615cb280d
volodymyr <volodymyr-desktop@MacBook-Pro-Admin.local>
2025-03-22 11:40:23 +0200

    QUIZ | create global error

---
+++ frontend/app/error.tsx
@@ -0,0 +1,16 @@
+
+export default function GlobalError({ error, reset }: { error: Error; reset: () => void }) {
+    return (
+        <html>
+            <body>
+                <h1>Something went wrong!</h1>
+                <p>{error.message}</p>
+                <button
+                    onClick={() => reset()} 
+                >
+                    Try Again
+                </button>
+            </body>
+        </html>
+    );
+}
